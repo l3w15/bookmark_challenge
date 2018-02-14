a86ad7b98541d12fb2d9e622b487ad2d2f6f59c3
Ben <ben.challenor@btopenworld.com>
2018-02-14 11:44:42 +0000

    Added unit test for adding link

--- spec/link_spec.rb
+++ spec/link_spec.rb
@@ -9,4 +9,12 @@ describe Link do
       expect(described_class.all).to include "http://facebook.com"
     end
   end
+
+  describe "#add" do
+    it "adds a link to the database" do
+      described_class.add("http://friedegg.com")
+      expect(described_class.all).to include "http://friedegg.com"
+    end
+  end
+
 end
