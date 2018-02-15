099cd3485ad081f0bc84c187f504bbb8a8f48cbc
Lewis Watson <lewwwiswatson@gmail.com>
2018-02-15 17:46:19 +0000

    add failing test for apostophes in title

--- spec/features/add_link_feature_spec.rb
+++ spec/features/add_link_feature_spec.rb
@@ -1,8 +1,10 @@
 feature "Add link" do
 
   let(:good_link) { double "a link and title", url: "http://www.friedegg.com",
-                    title: "friedegg"}
-  let(:bad_link) { "http:||www.foovar.com" }
+                    title: "friedegg" }
+  let(:bad_url) { "http:||www.foovar.com" }
+  let(:apostrophe_link_title) { double "a link and title with apostrophe",
+        url: "http://www.facebook.com", title: "don't use facebook" }
 
   scenario "User clicks on a button and is directed to add link form" do
     visit '/'
@@ -24,8 +26,18 @@ feature "Add link" do
   scenario "User gets a flash error message if the link is not a valid url" do
     visit '/'
     click_button "Add link"
-    fill_in("url", :with => bad_link)
+    fill_in("url", :with => bad_url)
     click_button "Submit link"
-    expect(page).to have_content "Sorry, \"#{bad_link}\" is not a valid link"
+    expect(page).to have_content "Sorry, \"#{bad_url}\" is not a valid link"
+  end
+
+  scenario "It allows a user to use apostrophes in a title" do
+    visit '/'
+    click_button "Add link"
+    fill_in("url", :with => apostrophe_link_title.url)
+    fill_in("title", :with => apostrophe_link_title.title)
+    click_button "Submit link"
+    expect(page).to have_link(apostrophe_link_title.title)
+    expect(page).not_to have_content "not a valid link"
   end
 end
